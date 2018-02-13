ccfdfafbe1b85c316d88d2884f2fdd0b29a79878
Gruniek <mous.be@gmail.com>
2018-02-13 11:55:01 +0100

    Update and rename install.sh to nixie.sh

--- nixie.sh
+++ nixie.sh
@@ -10,7 +10,7 @@ echo "##########################";
 echo " ";
 
 config_file="configuration.ini";
-
+dir_now=`pwd`;
 cd /tmp 
 wget https://raw.githubusercontent.com/Gruniek/nixie/master/version.txt
 version=`cat /tmp/version.txt`;
@@ -18,6 +18,10 @@ rm /tmp/version.txt
 wget https://raw.githubusercontent.com/Gruniek/nixie/master/$version/installer.sh
 chmod 777 installer.sh
 ./installer.sh
+rm -rf /tmp/installer.sh
+rm -rf /tmp/version.txt
+cd $dir_now;
+
 
 
 
